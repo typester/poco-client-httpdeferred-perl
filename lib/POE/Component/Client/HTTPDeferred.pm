da62f115c7ed00a7e861fe3c25ab4b26d8ee14d6
Daisuke Murase <typester@cpan.org>
2008-11-13 22:53:14 +0900

    quick implementation

--- lib/POE/Component/Client/HTTPDeferred.pm
+++ lib/POE/Component/Client/HTTPDeferred.pm
@@ -1,8 +1,25 @@
 package POE::Component::Client::HTTPDeferred;
-use strict;
-use warnings;
+use Moose;
 
 our $VERSION = '0.01';
+
+use POE qw/
+    Component::Client::HTTP
+    Component::Client::HTTPDeferred::Deferred
+    /;
+
+has client_alias => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => sub { 'ua' },
+);
+
+has session => (
+    is  => 'rw',
+    isa => 'POE::Session',
+);
+
+__PACKAGE__->meta->make_immutable;
 
 =head1 NAME
 
@@ -20,6 +37,89 @@ It looks like the author of the extension was negligent enough
 to leave the stub unedited.
 
 Blah blah blah.
+
+=head1 METHODS
+
+=head2 new
+
+=cut
+
+sub BUILD {
+    my $self = shift;
+
+    $self->{session} = POE::Session->create(
+        object_states => [
+            $self => {
+                map { $_ => "poe_$_" } qw/_start request response/
+            },
+        ],
+    );
+}
+
+=head2 request
+
+=cut
+
+sub request {
+    my ($self, $req) = @_;
+
+    my $d = $req->{_deferred} = POE::Component::Client::HTTPDeferred::Deferred->new(
+        request      => $req,
+        client_alias => $self->client_alias,
+    );
+
+    $poe_kernel->post( $self->session->ID => request => $req );
+
+    $d;
+}
+
+=head2 shutdown
+
+=cut
+
+sub shutdown {
+    my $self = shift;
+    $poe_kernel->post( $self->client_alias => 'shutdown' );
+}
+
+=head1 POE METHODS
+
+=head2 poe__start
+
+=cut
+
+sub poe__start {
+    my ($self, $kernel) = @_[OBJECT, KERNEL];
+    POE::Component::Client::HTTP->spawn( Alias => $self->client_alias );
+}
+
+=head2 poe_request
+
+=cut
+
+sub poe_request {
+    my ($self, $kernel, $req) = @_[OBJECT, KERNEL, ARG0];
+
+    $kernel->post( $self->client_alias, 'request', 'response', $req );
+}
+
+=head2 poe_response
+
+=cut
+
+sub poe_response {
+    my ($self, $kernel) = @_[OBJECT, KERNEL];
+    my ($req, $res)     = ($_[ARG0]->[0], $_[ARG1]->[0]);
+
+    my $d = delete $req->{_deferred} or confess 'deferred object not found';
+
+    if ($res->is_success) {
+        $d->callback($res);
+    }
+    else {
+        $d->errback($res);
+    }
+}
 
 =head1 AUTHOR
 
---
+++ lib/POE/Component/Client/HTTPDeferred/Deferred.pm
@@ -0,0 +1,72 @@
+package POE::Component::Client::HTTPDeferred::Deferred;
+use Moose;
+
+use POE;
+
+has request => (
+    is       => 'rw',
+    isa      => 'HTTP::Request',
+    weak_ref => 1,
+    required => 1,
+);
+
+has client_alias => (
+    is       => 'rw',
+    isa      => 'Str',
+    required => 1,
+);
+
+has callbacks => (
+    is      => 'rw',
+    isa     => 'ArrayRef',
+    lazy    => 1,
+    default => sub { [] },
+);
+
+sub cancel {
+    my $self = shift;
+    $poe_kernel->post( $self->client_alias => cancel => $self->request );
+
+    $self;
+}
+
+sub callback {
+    my ($self, $res) = @_;
+
+    for my $cb (@{ $self->callbacks }) {
+        $cb->[0]->($res) if $cb->[0];
+    }
+}
+
+sub errback {
+    my ($self, $res) = @_;
+
+    for my $cb (@{ $self->callbacks }) {
+        $cb->[1]->($res) if $cb->[1];
+    }
+}
+
+sub addBoth {
+    my ($self, $cb) = @_;
+    $self->addCallbacks($cb, $cb);
+}
+
+sub addCallback {
+    my ($self, $cb) = @_;
+    $self->addCallbacks($cb, undef);
+}
+
+
+sub addCallbacks {
+    my ($self, $cb, $eb) = @_;
+    push @{ $self->callbacks }, [ $cb, $eb ];
+
+    $self;
+}
+
+sub addErrback {
+    my ($self, $eb) = @_;
+    $self->addCallbacks(undef, $eb);
+}
+
+1;
